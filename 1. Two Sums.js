7b493c8526fc12d95afac5c1fe9e852882803007
hitchcliff <hitchcliff1@gmail.com>
2025-05-31 02:47:14 +0800

    improving 2 sum with hashmap

--- 1. Two Sums.js
+++ 1. Two Sums.js
@@ -1,20 +1,36 @@
-// const nums = [2, 7, 11, 15];
+var twoSum = function (nums, target) {
+  const hashMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const diff = target - num;
+
+    if (diff in hashMap) {
+      return [i, hashMap[diff]];
+    }
+
+    hashMap[num] = i;
+  }
+};
+
+// const nums = [2, 11, 7, 15];
+// const target = 9;
 // const nums = [3, 3];
 const nums = [2, 5, 5, 11];
 const target = 10;
 
-twoSums(nums, target);
+console.log(twoSum(nums, target));
 
-function twoSums(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    const cur = nums[i];
-    const x = target - cur;
+// function twoSums(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     const cur = nums[i];
+//     const x = target - cur;
 
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] === x) {
-        console.log(x, [i, j]);
-        return [i, j];
-      }
-    }
-  }
-}
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[j] === x) {
+//         console.log(x, [i, j]);
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
