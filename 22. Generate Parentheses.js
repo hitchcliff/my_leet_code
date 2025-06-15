f06aeb2838ecf1457234aa657d5f01922bb2daf8
hitchcliff <hitchcliff1@gmail.com>
2025-06-15 11:02:42 +0800

    generating parentheses

---
+++ 22. Generate Parentheses.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  console.log(n);
+};
+
+Input: n = 3;
+Output: ["((()))", "(()())", "(())()", "()(())", "()()()"];
+
+Input: n = 1;
+Output: ["()"];
+
+console.log(generateParenthesis(n));
